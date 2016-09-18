2d96ef10c14c3faf5c3ae050f8b33d3c16307d02
noisemaster <psp1001k2006@gmail.com>
2016-09-18 19:17:10 -0400

    Added Morbotron tests

--- frinkiac_test.go
+++ frinkiac_test.go
@@ -15,7 +15,29 @@ func TestFrame(t *testing.T) {
 
 func TestMeme(t *testing.T) {
 	final := "https://frinkiac.com/meme/S07E21/579294.jpg?b64lines=YW5kIEkndmUgbmV2ZXIgaGVhcmQKYW55b25lIHVzZSB0aGUgcGhyYXNlCiJzdGVhbWVkIGhhbXMuIg=="
-	recv, err := GetFrinkiacFrameAndCaption("Steamed Hams")
+	recv, err := GetFrinkiacMeme("Steamed Hams")
+	if err != nil {
+		t.Error(err)
+	}
+	if recv != final {
+		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
+	}
+}
+
+func TestMorboFrame(t *testing.T) {
+	final := "https://morbotron.com/img/S01E05/1112308.jpg"
+	recv, err := GetMorbotronFrame("this planet")
+	if err != nil {
+		t.Error(err)
+	}
+	if recv != final {
+		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
+	}
+}
+
+func TestMorboMeme(t *testing.T) {
+	final := "https://morbotron.com/meme/S01E05/1112308.jpg?b64lines=VGhhdCB3YXMganVzdCBhIHNob3cgZm9yCnRoZSBwdWJsaWMu"
+	recv, err := GetMorbotronMeme("this planet")
 	if err != nil {
 		t.Error(err)
 	}
