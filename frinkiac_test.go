849ced333aa9fd50c1dac21ea814b138129da7ca
noisemaster <psp1001k2006@gmail.com>
2016-09-18 02:34:06 -0400

    Testing Frinkiac Meme

--- frinkiac_test.go
+++ frinkiac_test.go
@@ -12,3 +12,14 @@ func TestFrame(t *testing.T) {
 		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
 	}
 }
+
+func TestMeme(t *testing.T) {
+	final := "https://frinkiac.com/img/S07E21/579294.jpg"
+	recv, err := GetFrinkiacFrameAndCaption("Steamed Hams")
+	if err != nil {
+		t.Error(err)
+	}
+	if recv != final {
+		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
+	}
+}
