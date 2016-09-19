656aa8eb18b41fd94fc110bec3b3661479fa0f04
noisemaster <psp1001k2006@gmail.com>
2016-09-18 20:14:06 -0400

    Added Gif Support

--- frinkiac.go
+++ frinkiac.go
@@ -142,6 +142,26 @@ func GetFrinkiacMeme(query string) (string, error) {
 	return "https://frinkiac.com/meme/" + frames[0].Episode + "/" + strconv.Itoa(frames[0].Timestamp) + ".jpg?b64lines=" + uEnc, nil
 }
 
+//GetFrinkiacGifMeme Returns a URL for a GIF with a caption
+func GetFrinkiacGifMeme(query string) (string, error) {
+	var cap string
+	frames, err := getFrinkiacFrameData(query)
+	if err != nil {
+		return "", err
+	}
+	info, err := getFrinkiacEpisodeInfo(frames[0])
+	if err != nil {
+		return "", err
+	}
+	for _, v := range info.Subtitles {
+		cap += v.Content + "\n"
+	}
+	cap = strings.TrimSuffix(cap, "\n")
+	cap = wordwrap.WrapString(cap, 25)
+	uEnc := base64.URLEncoding.EncodeToString([]byte(cap))
+	return "https://frinkiac.com/gif/" + info.Frame.Episode + "/" + strconv.Itoa(info.Subtitles[0].StartTimestamp) + "/" + strconv.Itoa(info.Subtitles[len(info.Subtitles)-1].EndTimestamp) + ".gif?b64lines=" + uEnc, nil
+}
+
 //GetMorbotronFrame Sends a URL of a frame from Morbotron
 func GetMorbotronFrame(query string) (string, error) {
 	frames, err := getMorbotronFrameData(query)
@@ -164,4 +184,24 @@ func GetMorbotronMeme(query string) (string, error) {
 	cap := wordwrap.WrapString(info.Subtitles[0].Content, 25)
 	uEnc := base64.URLEncoding.EncodeToString([]byte(cap))
 	return "https://morbotron.com/meme/" + frames[0].Episode + "/" + strconv.Itoa(frames[0].Timestamp) + ".jpg?b64lines=" + uEnc, nil
+}
+
+//GetMorbotronGifMeme Returns a URL for a GIF with a caption
+func GetMorbotronGifMeme(query string) (string, error) {
+	var cap string
+	frames, err := getMorbotronFrameData(query)
+	if err != nil {
+		return "", err
+	}
+	info, err := getMorbotronEpisodeInfo(frames[0])
+	if err != nil {
+		return "", err
+	}
+	for _, v := range info.Subtitles {
+		cap += v.Content + "\n"
+	}
+	cap = strings.TrimSuffix(cap, "\n")
+	cap = wordwrap.WrapString(cap, 25)
+	uEnc := base64.URLEncoding.EncodeToString([]byte(cap))
+	return "https://morbotron.com/gif/" + info.Frame.Episode + "/" + strconv.Itoa(info.Subtitles[0].StartTimestamp) + "/" + strconv.Itoa(info.Subtitles[len(info.Subtitles)-1].EndTimestamp) + ".gif?b64lines=" + uEnc, nil
 }
--- frinkiac_test.go
+++ frinkiac_test.go
@@ -24,6 +24,17 @@ func TestMeme(t *testing.T) {
 	}
 }
 
+func TestGifMeme(t *testing.T) {
+	final := "https://frinkiac.com/gif/S07E21/576200/581967.gif?b64lines=YW5kIEkndmUgbmV2ZXIgaGVhcmQKYW55b25lIHVzZSB0aGUgcGhyYXNlCiJzdGVhbWVkIGhhbXMuIgpPaCwgbm90IGluIFV0aWNhLCBubywKaXQncyBhbiBBbGJhbnkKZXhwcmVzc2lvbi4="
+	recv, err := GetFrinkiacGifMeme("Steamed Hams")
+	if err != nil {
+		t.Error(err)
+	}
+	if recv != final {
+		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
+	}
+}
+
 func TestMorboFrame(t *testing.T) {
 	final := "https://morbotron.com/img/S01E05/1112308.jpg"
 	recv, err := GetMorbotronFrame("this planet")
@@ -38,6 +49,17 @@ func TestMorboFrame(t *testing.T) {
 func TestMorboMeme(t *testing.T) {
 	final := "https://morbotron.com/meme/S01E05/1112308.jpg?b64lines=VGhhdCB3YXMganVzdCBhIHNob3cgZm9yCnRoZSBwdWJsaWMu"
 	recv, err := GetMorbotronMeme("this planet")
+	if err != nil {
+		t.Error(err)
+	}
+	if recv != final {
+		t.Errorf("Recv: %s \n Want: %s \n %s\n", recv, final, "URLs were not equal")
+	}
+}
+
+func TestMorboGifMeme(t *testing.T) {
+	final := "https://morbotron.com/gif/S01E05/1108431/1115063.gif?b64lines=VGhhdCB3YXMganVzdCBhIHNob3cgZm9yCnRoZSBwdWJsaWMuCldlIGFyZSB0aGUgdHJ1ZSBydWxlcnMgb2YKdGhpcyBwbGFuZXQuCkhhbmQtY2FydmVkIGZyb20KbWV0ZW9yaXRlcyBieSB0aGUgcm9ib3QKZm91bmRlcnM="
+	recv, err := GetMorbotronGifMeme("this planet")
 	if err != nil {
 		t.Error(err)
 	}
